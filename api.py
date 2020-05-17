8d2678a84abb72506c11e3b5212d3a4f7496a6b6
aftiyah <abd2010ly@gmail.com>
2020-05-17 09:27:03 +0200

    Add gitignore and requirements.txt.

--- api.py
+++ api.py
@@ -4,9 +4,17 @@ from sqlalchemy import exc
 import json
 from flask_cors import CORS
 
-from .database.models import db_drop_and_create_all, setup_db, Drink
-from .auth.auth import AuthError, requires_auth
+from database.models import db_drop_and_create_all, setup_db, Sheets
+from auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
 CORS(app)
+
+db_drop_and_create_all()
+
+@app.route('/sheets')
+def get_sheets():
+    selection = sheets.query.order_by(sheets.id).all()
+
+    return "EE434"
