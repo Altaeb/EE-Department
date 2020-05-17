1e923410753beab8de0757987600c700c953af68
aftiyah <abd2010ly@gmail.com>
2020-05-17 09:13:47 +0200

    Build models in database.

--- database/models.py
+++ database/models.py
@@ -18,3 +18,37 @@ def setup_db(app):
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
+
+class Movie(db.Model):
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(Integer)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+
+class Actor(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
